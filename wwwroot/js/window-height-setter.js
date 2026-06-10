2d1c5de8cba328943c6a7e2707d5885a645a21e4
Alex <alexwestsimple@mail.ru>
2026-06-10 23:04:29 +0300

    try fix height

---
+++ wwwroot/js/window-height-setter.js
@@ -0,0 +1,8 @@
+﻿function fixDynamicHeight() {
+    const realHeight = window.innerHeight;
+    document.documentElement.style.setProperty('--pwa-height', `${realHeight}px`);
+}
+
+window.addEventListener('DOMContentLoaded', fixDynamicHeight);
+window.addEventListener('resize', fixDynamicHeight);
+window.addEventListener('orientationchange', fixDynamicHeight);
