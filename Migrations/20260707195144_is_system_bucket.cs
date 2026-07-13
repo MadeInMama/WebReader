948a653ee17b018d6c2be44ede69beae0082d027
Alex <alexwestsimple@mail.ru>
2026-07-13 13:23:20 +0300

    add migrations

---
+++ Migrations/20260707195144_is_system_bucket.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebReader.Migrations
+{
+    /// <inheritdoc />
+    public partial class is_system_bucket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSystem",
+                table: "Buckets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSystem",
+                table: "Buckets");
+        }
+    }
+}
